3f61f3d895973436329aee2e857e28c2c51b60e1
EbYVarghese18 <ebyblr2k18@gmail.com>
2022-11-15 23:04:28 +0530

    final

--- login/authentication/views.py
+++ login/authentication/views.py
@@ -5,31 +5,32 @@ from django.contrib import messages
 
 # Create your views here.
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
-def signin(request):
-    if 'username' in request.session:
-        return redirect(index) 
+def signin(request):        
+    if 'usersession' in request.session:
+        return redirect('index')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
 
         if user is not None:
-            request.session['username'] = username
-            return redirect('index')
+            request.session['usersession'] = username     #creating session 
+            return redirect('index') 
         else:
             messages.info(request, 'Invalid credentials')
-            return redirect('signin')
+            # return redirect(signin)   
+            return render(request, 'signin.html') 
     else:
-        return render(request, 'signin.html')  
+        return render(request, 'signin.html')
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def index(request):
-    if 'username' in request.session:
+    if 'usersession' in request.session:
         return render(request, 'index.html')
     return redirect('signin')
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def signout(request):
-    if 'username' in request.session:
-        request.session.flush() 
+    if 'usersession' in request.session:
+        request.session.flush()
     return redirect('signin')
